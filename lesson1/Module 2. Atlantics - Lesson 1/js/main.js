6304220b80671137f71e1f4f1c8fafc7c8e6f6de
Alex-Tsvetanov <alex@tsalex.tk>
2018-11-14 09:13:59 +0200

    lesson 2

---
+++ lesson1/Module 2. Atlantics - Lesson 1/js/main.js
@@ -0,0 +1,10 @@
+
+// Inicializating the global instance for the game, which is just an object from type Phaser.Game.
+// Usually we have tree more parameters. In this case we do not need them.
+var game = new Phaser.Game(1200, 1200, Phaser.AUTO);
+
+// Adding all of the states.
+game.state.add('GameState', GameState);
+
+// Starting the first State.
+game.state.start('GameState');
---
+++ lesson1/Module 2. Atlantics - Lesson 1/js/states/GameState.js
@@ -0,0 +1,40 @@
+
+// Creating a global variable for layers array.
+var layers = [];
+
+var GameState = { 
+
+    preload: function() {
+
+        // Setting the scaling mode for the Game State.
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+
+        // Loading the background image.
+        this.game.load.image('background', 'assets/images/background.jpg');
+
+        // Loading the map in JSON and the tileset for the tilemap as a image.
+        this.game.load.tilemap('tilemap', 'assets/maps/tilemap.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tileset', 'assets/maps/tileset.png', 16, 16);
+
+    },
+
+    create: function() {
+
+        // Setting the background as a image.
+        this.background = this.game.add.sprite(0, 0, 'background');
+
+        // Adding the map in JSON and the tileset for the tilemap as a image.
+        this.map = this.game.add.tilemap('tilemap');
+        this.map.addTilesetImage('tileset', 'tileset');
+
+        // Creating all of the layers. Could be done in an array, containing all of the layers. Could be done in a single variable.
+        // !!! The order is important and it could be different from the order in the JSON file. In this case IT IS.
+
+        layers[3] = this.map.createLayer('3');   
+        layers[0] = this.map.createLayer('0');
+        layers[1] = this.map.createLayer('1');
+        layers[2] = this.map.createLayer('2');
+    }       
+};
